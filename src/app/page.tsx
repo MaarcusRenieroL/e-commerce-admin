e7690acdeae4f8775ab7141ced713bb727428009
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-24 15:35:24 +0530

    init: init shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center h-screen w-full">
       <h1 className="text-2xl font-bold">E Commerce Admin Dashboard</h1>
+      <Button>Clcik me</Button>
     </div>
   );
 }
