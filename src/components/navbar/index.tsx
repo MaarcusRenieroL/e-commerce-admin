6f8ce7cb19ae84113824683a2af118ecc63fb6d0
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-26 13:40:49 +0530

    feat: add account nav

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,9 +2,17 @@ import { FC } from "react";
 import { StoreSwitcher } from "./store-switcher";
 import { server } from "~/lib/trpc/server";
 import { RoutesNavbar } from "./routes-nav";
+import { Account } from "./account";
+import { getServerAuthSession } from "~/lib/auth";
 
 export const Navbar: FC = async () => {
   const stores = await server.store.getStores();
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return;
+  }
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center justify-between px-10 py-6 space-x-5">
@@ -12,7 +20,7 @@ export const Navbar: FC = async () => {
           <StoreSwitcher items={stores} />
           <RoutesNavbar />
         </div>
-        <div>user</div>
+        <Account email={session.user.email ?? ""} />
       </div>
     </div>
   );
